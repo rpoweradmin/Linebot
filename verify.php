b70c4873d0014157c0e76b0a742c57407c82d8f2
rpoweradmin <rpoweradmin@gmail.com>
2017-09-12 17:03:52 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,5 +1,5 @@
 <?php
-$access_token = 'XXXXXXXXXXXXXXXXXXXXXXX';
+$access_token = 'FG8vOTKY9L+F2IsYjOF2/ZJziHRALbcaqmHZ8ZexP0+IvJpW03L4E+OQ3afE8chL2zEPqLg0tJ7czXeYZE7H+Z78Yh9M2x3vmeo/tpGcMbv5kes5R2KxIFJs+l+vNMjzXobKjwjldiMwos6SHz1kQwdB04t89/1O/w1cDnyilFU=';
 
 $url = 'https://api.line.me/v1/oauth/verify';
 
